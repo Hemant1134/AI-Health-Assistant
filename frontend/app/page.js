1306f6c0927c550774eeff46ed964105b12dc5d6
unknown <hemantpal2004@gmail.com>
2025-12-06 00:15:07 +0530

    code updated

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "@/context/AuthContext";
 import { Box } from "@mui/material";
 import AuthScreen from "@/components/AuthScreen";
